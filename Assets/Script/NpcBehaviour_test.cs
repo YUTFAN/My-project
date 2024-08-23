3693cb4f7d7e7e6ed424aa59bb5a885a9a8df427
haochengw1 <127472500+haochengw1@users.noreply.github.com>
2024-08-23 19:25:01 +1000

    update mediaplayer feature

---
+++ Assets/Script/NpcBehaviour_test.cs
@@ -0,0 +1,20 @@
+// using System.Collections;
+// using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcBehaviour_test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // Log message of Npc status
+        Debug.Log("Npc Successfully Loaded");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
